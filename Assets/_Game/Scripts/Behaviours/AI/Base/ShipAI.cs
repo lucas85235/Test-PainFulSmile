a9c8540be74a0dc979a7ba8d7465bbed00d4aeb5
Lucas Lima <lucas85235@hotmail.com>
2023-01-25 18:45:29 -0400

    Add damage to enemys

--- Assets/_Game/Scripts/Behaviours/AI/Base/ShipAI.cs
+++ Assets/_Game/Scripts/Behaviours/AI/Base/ShipAI.cs
@@ -6,12 +6,15 @@ public class ShipAI : MonoBehaviour
 {
     [SerializeField] protected float followSpeed = 2f;
     [SerializeField] protected int damage = 10;
+    [SerializeField] protected Life life;
 
+    public Life Life { get => life; }
     protected GameObject _player;
 
     protected virtual void Start()
     {
         _player = GameObject.FindWithTag("Player");
+        life.OnDeath.AddListener(OnDeath);
     }
 
     protected void FollowPlayer()
@@ -26,6 +29,11 @@ public class ShipAI : MonoBehaviour
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * followSpeed);
+    }
+
+    private void OnDeath()
+    {
+        Destroy(gameObject);
     }
 
     protected virtual void OnDestroy()
---
+++ Assets/_Game/Scripts/Behaviours/Bullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private Target targetTag = Target.Player;
+    [SerializeField] private int damage = 15;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(targetTag.ToString()))
+        {
+            if (targetTag == Target.Player && other.TryGetComponent(out Player player))
+            {
+                player.Life.TakeDamage(damage);
+            }
+            if (targetTag == Target.Enemy && other.TryGetComponent(out ShipAI ai))
+            {
+                ai.Life.TakeDamage(damage);
+            }
+        }
+    }
+
+    private enum Target
+    {
+        Player,
+        Enemy
+    }
+}
